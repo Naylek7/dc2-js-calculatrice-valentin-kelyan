5cf940b15a0f19d104f2952cb7c185b46dea18b8
Kelyan valentin <kelyanvalentin@MacBook-Pro-de-Kelyan.local>
2022-11-22 11:25:29 +0100

    calculatrice js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var a;
+var b;
+var c;
+
+function sum(a, b){
+    c = a + b;
+}
+
+function subs(a, b){
+    c = a - b;
+}
+
+function div(a, b){
+    c = a / b;
+}
+
+function mult(a, b){
+    c = a * b;
+}
+
+sum(4,3)
+console.log(c);
